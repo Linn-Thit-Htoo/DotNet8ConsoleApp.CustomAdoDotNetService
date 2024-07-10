41d7883ad063d85672994f39a4b16aa6b7dc464d
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-10 12:04:28 +0630

    Format Code

--- DotNet8ConsoleApp.CustomAdoDotNetService/Query.cs
+++ DotNet8ConsoleApp.CustomAdoDotNetService/Query.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8ConsoleApp.CustomAdoDotNetService;
 
-namespace DotNet8ConsoleApp.CustomAdoDotNetService
+public class Query
 {
-    public class Query
-    {
-        public static string GetAllBlogsQuery { get; } = @"SELECT BlogId, BlogTitle, BlogAuthor, BlogContent FROM Tbl_Blog
+    public static string GetAllBlogsQuery { get; } =
+        @"SELECT BlogId, BlogTitle, BlogAuthor, BlogContent FROM Tbl_Blog
 ORDER BY BlogId DESC";
-        public static string CreateBlogQuery { get; } = @"INSERT INTO Tbl_Blog (BlogTitle, BlogAuthor, BlogContent)
+    public static string CreateBlogQuery { get; } =
+        @"INSERT INTO Tbl_Blog (BlogTitle, BlogAuthor, BlogContent)
 VALUES(@BlogTitle, @BlogAuthor, @BlogContent)";
-    }
 }
