e55843652edce2bbcd7254ff2e7f9efd004e2c10
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-10 11:59:56 +0630

    Add project files.

---
+++ DotNet8ConsoleApp.CustomAdoDotNetService/AdoDotNetService.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8ConsoleApp.CustomAdoDotNetService
+{
+    public class AdoDotNetService
+    {
+        public readonly string _connStr = "Data Source=.;Database=testDb;User ID=sa;Password=sasa@123";
+        public async Task<List<T>> QueryAsync<T>(string query, SqlParameter[]? parameters = null, SqlTransaction? transaction = null)
+        {
+            SqlConnection conn = GetConnection();
+            await conn.OpenAsync();
+            SqlCommand cmd = new(query, conn, transaction);
+            if (parameters is not null)
+            {
+                cmd.Parameters.AddRange(parameters);
+            }
+            SqlDataAdapter adapter = new(cmd);
+            DataTable dt = new();
+            adapter.Fill(dt);
+            await conn.CloseAsync();
+
+            string jsonStr = JsonConvert.SerializeObject(dt);
+            var lst = JsonConvert.DeserializeObject<List<T>>(jsonStr)!;
+
+            return lst;
+        }
+
+        public async Task<DataTable> QueryFirstOrDefaultAsync(string query, SqlParameter[]? parameters = null, SqlTransaction? transaction = null)
+        {
+            SqlConnection conn = GetConnection();
+            await conn.OpenAsync();
+            SqlCommand cmd = new(query, conn, transaction);
+            if (parameters is not null)
+            {
+                cmd.Parameters.AddRange(parameters);
+            }
+            SqlDataAdapter adapter = new(cmd);
+            DataTable dt = new();
+            adapter.Fill(dt);
+            await conn.CloseAsync();
+
+            return dt;
+        }
+
+        public async Task<int> ExecuteAsync(string query, SqlParameter[]? parameters = null, SqlTransaction? transaction = null)
+        {
+            SqlConnection conn = GetConnection();
+            await conn.OpenAsync();
+            SqlCommand cmd = new(query, conn, transaction);
+            if (parameters is not null)
+            {
+                cmd.Parameters.AddRange(parameters);
+            }
+            int result = await cmd.ExecuteNonQueryAsync();
+            await conn.CloseAsync();
+
+            return result;
+        }
+
+        private SqlConnection GetConnection() => new(_connStr);
+    }
+}
---
+++ DotNet8ConsoleApp.CustomAdoDotNetService/Program.cs
@@ -0,0 +1,72 @@
+﻿using DotNet8ConsoleApp.CustomAdoDotNetService;
+using System.Data.SqlClient;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        await Read();
+        //await Create("Sample Title", "Sample Author", "Sample Content");
+    }
+
+    public static async Task Read()
+    {
+        try
+        {
+            AdoDotNetService adoDotNetService = new();
+            string query = @"SELECT BlogId, BlogTitle, BlogAuthor, BlogContent FROM Tbl_Blog
+ORDER BY BlogId DESC";
+            List<BlogModel> lst = await adoDotNetService.QueryAsync<BlogModel>(query);
+
+            foreach (var item in lst)
+            {
+                Console.WriteLine($"Blog Id: {item.BlogId}");
+                Console.WriteLine($"Blog Title: {item.BlogTitle}");
+                Console.WriteLine($"Blog Author: {item.BlogAuthor}");
+                Console.WriteLine($"Blog Content: {item.BlogContent}");
+            }
+        }
+        catch (CustomException ex)
+        {
+            throw new CustomException(ex.Message);
+        }
+    }
+
+    public static async Task Create(string blogTitle, string blogAuthor, string blogContent)
+    {
+        try
+        {
+            AdoDotNetService adoDotNetService = new();
+            string query = @"INSERT INTO Tbl_Blog (BlogTitle, BlogAuthor, BlogContent)
+VALUES(@BlogTitle, @BlogAuthor, @BlogContent)";
+            List<SqlParameter> parameters = new()
+            {
+                new("@BlogTitle", blogTitle),
+                new("@BlogAuthor", blogAuthor),
+                new("@BlogContent", blogContent)
+            };
+            int result = await adoDotNetService.ExecuteAsync(query, parameters.ToArray());
+
+            Console.WriteLine(result > 0 ? "Saving Successful." : "Saving Fail.");
+        }
+        catch (CustomException ex)
+        {
+            throw new CustomException(ex.Message);
+        }
+    }
+}
+
+public class BlogModel
+{
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+}
+
+public class CustomException : Exception
+{
+    public CustomException(string? message) : base(message)
+    {
+    }
+}
